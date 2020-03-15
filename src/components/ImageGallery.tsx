3d032c890ee5e48437e15ba5696e4d01dfcba0c4
Barney Loosemore <barney@barney.co.uk>
2020-03-15 19:11:21 +0000

    Add text & animations to offer gallery

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,43 +1,95 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import {
   StyleSheet,
   Image,
-  Text,
-  TouchableOpacity,
-  TouchableHighlight
+  TouchableHighlight,
+  View,
+  Animated
 } from "react-native";
 import Carousel from "react-native-snap-carousel";
 
 import { GalleryIndex } from "./GalleryIndex";
 import {
+  HORIZONTAL_MARGIN,
   GALLERY_IMAGE_WIDTH,
   GALLERY_IMAGE_HEIGHT,
   SLIDER_WIDTH
 } from "../constants/styles";
 
-type ImageGalleryProps = { images: string[] };
+type ImageGalleryProps = {
+  offers: {
+    image: string;
+    offerText: string;
+  }[];
+};
 
-export const ImageGallery = ({ images }: ImageGalleryProps) => {
+type ContentProps = { image: string; offerText: string; active: boolean };
+
+const Content = ({ image, offerText, active }: ContentProps) => {
+  const opacity = new Animated.Value(0);
+
+  useEffect(() => {
+    if (active) {
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 200,
+        delay: 0
+      }).start();
+    }
+    if (!active) {
+      opacity.setValue(0);
+    }
+  }, [active]);
+
+  return (
+    <View>
+      <Animated.Text
+        style={{
+          position: "absolute",
+          color: "white",
+          fontSize: 24,
+          bottom: 45,
+          left: opacity.interpolate({
+            inputRange: [0, 1],
+            outputRange: [-10, HORIZONTAL_MARGIN]
+          }),
+          opacity,
+          zIndex: 999
+        }}>
+        {offerText}
+      </Animated.Text>
+      <Image source={{ uri: image }} style={styles.image} />
+    </View>
+  );
+};
+
+export const ImageGallery = ({ offers }: ImageGalleryProps) => {
   const carouselRef = useRef(null);
   const [activeIndex, setActiveIndex] = useState(0);
 
+  console.log(activeIndex);
+
   return (
     <>
-      <GalleryIndex galleryLength={images.length} activeIndex={activeIndex} />
+      <GalleryIndex galleryLength={offers.length} activeIndex={activeIndex} />
       <Carousel
         ref={carouselRef}
         itemWidth={GALLERY_IMAGE_WIDTH}
         sliderWidth={SLIDER_WIDTH}
-        data={images}
+        data={offers}
         loop
         loopClonesPerSide={1}
         onSnapToItem={() => setActiveIndex(carouselRef.current.currentIndex)}
         autoplay
         autoplayDelay={2000}
         autoplayInterval={6000}
-        renderItem={({ item, index }) => (
-          <TouchableHighlight key={index}>
-            <Image source={{ uri: item }} style={styles.image} />
+        renderItem={({ item: { image, offerText }, index }) => (
+          <TouchableHighlight key={index} style={{ backgroundColor: "black" }}>
+            <Content
+              image={image}
+              offerText={offerText}
+              active={index - 1 === activeIndex}
+            />
           </TouchableHighlight>
         )}
       />
@@ -48,6 +100,7 @@ export const ImageGallery = ({ images }: ImageGalleryProps) => {
 const styles = StyleSheet.create({
   image: {
     height: GALLERY_IMAGE_HEIGHT,
-    width: GALLERY_IMAGE_WIDTH
+    width: GALLERY_IMAGE_WIDTH,
+    opacity: 0.6
   }
 });
--- src/static/mocks.ts
+++ src/static/mocks.ts
@@ -1,10 +1,23 @@
 import Icon from "./icons";
 
-const images = [
-  "https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg",
-  "https://images.squarespace-cdn.com/content/564572f1e4b00444404f386c/1475868875058-2NSGUDJ1L70GVXPNBHNW/MoroccanFood-44.jpg?content-type=image%2Fjpeg",
-  "https://images.squarespace-cdn.com/content/v1/54d4153ce4b00c0e483c13a6/1517485882974-GXWPUCO94OO0BQ1H3V43/ke17ZwdGBToddI8pDm48kKAwwdAfKsTlKsCcElEApLR7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UegTYNQkRo-Jk4EWsyBNhwKrKLo5CceA1-Tdpfgyxoog5ck0MD3_q0rY3jFJjjoLbQ/IMG_4990.jpg"
+const offers = [
+  {
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg",
+    offerText: "Get 50% off all pizzas"
+  },
+  {
+    image:
+      "https://images.squarespace-cdn.com/content/564572f1e4b00444404f386c/1475868875058-2NSGUDJ1L70GVXPNBHNW/MoroccanFood-44.jpg?content-type=image%2Fjpeg",
+    offerText: "Use code FD4U for free delivery"
+  },
+  {
+    image:
+      "https://images.squarespace-cdn.com/content/v1/54d4153ce4b00c0e483c13a6/1517485882974-GXWPUCO94OO0BQ1H3V43/ke17ZwdGBToddI8pDm48kKAwwdAfKsTlKsCcElEApLR7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UegTYNQkRo-Jk4EWsyBNhwKrKLo5CceA1-Tdpfgyxoog5ck0MD3_q0rY3jFJjjoLbQ/IMG_4990.jpg",
+    offerText: "£5 when you spend £25 or more"
+  }
 ];
+
 const restaurantCategories = [
   {
     title: "Favourites",
@@ -895,4 +908,4 @@ const cuisines = [
   { name: "Desserts", id: 8 }
 ];
 
-export { images, restaurants, restaurantCategories };
+export { offers, restaurants, restaurantCategories };
